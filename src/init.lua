e23bea81d157ea947d8d511487d68d0de4643613
Harry <otgo@outlook.es>
2019-06-04 01:29:59 -0500

    Update init.lua

--- src/init.lua
+++ src/init.lua
@@ -1,1 +1,1 @@
-local libs = {'api', 'utils'} local _ = {} for i,x in next, libs do _[x] = require( ('telegram.%s') : format (x) ) end return _
+local libs = {'api', 'utils'} local _ = {} for i,x in next,libs do _[x] = require( ('telegram.%s') : format (x) ) end return _
